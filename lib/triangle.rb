428072764866aaf8cc084340a43bf014191fe51f
Matthew Sherman <msherman0801@gmail.com>
2018-11-29 02:30:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,47 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  @@args = []
+  
+  def initialize(arg1, arg2, arg3)
+    @arg1 = arg1
+    @arg2 = arg2
+    @arg3 = arg3
+    @@args << arg1
+    @@args << arg2
+    @@args << arg3
+  end
+  
+  
+  def triangle?
+    @@args.each do |i| 
+      if i == 0 || i < 0 
+        return false 
+      end
+    end
+    true 
+  end
+  
+  
+  def kind
+  if triangle?
+    binding.pry
+    if @arg1 == @arg2 && @arg1 == @arg3
+      return :equilateral
+    elsif @arg1 == @arg2 || @arg1 == @arg3 || @arg2 == @arg3
+      return :isosceles
+    elsif @arg1 != @arg3 && @arg1 != @arg2
+      return :scalene
+    elsif @arg1 == 0 || @arg2 == 0 || @arg3 == 0
+      raise TriangleError
+    end
+  end
+  end
+  
+  class TriangleError < StandardError
+  end
+  
+  def assert_kind_of
+  end
 end
